f887c042767faf6a36c9657a43bbc68ba82b7ff3
Widiasa Mahendra <dmforaname@gmail.com>
2022-02-22 09:48:58 +0700

    base repository

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Repositories;
+//use Illuminate\Database\Eloquent\Model;
+use App\Models\Division;
+
+/**
+ * Class BaseRepository
+ *
+ * @package App\Repositories
+ */
+class BaseRepository implements RepositoryInterface
+{
+
+    /**      
+     * @var Model      
+     */     
+    protected $model;   
+
+    /**
+     * BaseRepository constructor.
+     *
+     * @param  object  $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Save entry
+     *
+     * @return mixed
+     */
+    public function save(array $attributes)
+    {
+        return $this->model->fill($attributes)->save();
+    }
+
+    /**
+     * Create entry
+     *
+     * @param  array  $attributes
+     *
+     * @return mixed
+     */
+    public function create(array $attributes)
+    {
+        return $this->model->create($attributes);
+    }
+
+    /**
+     * Query
+     *
+     * @return mixed
+     */
+    public function query()
+    {
+        return $this->model->query();
+    }
+
+    /**
+     * @param  int  $id
+     *
+     * @return mixed
+     */
+    public function getById(int $id)
+    {
+        return $this->model->find($id);
+    }
+
+    /**
+     * @param  int  $id
+     *
+     * @return mixed
+     */
+    public function firstOrCreate(array $first, array $create)
+    {
+        return $this->model->firstOrCreate($first,$create);
+    }
+}
---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repositories;
+
+/**
+ * Interface RepositoryInterface
+ *
+ * @package App\Repositories
+ */
+interface RepositoryInterface
+{
+    /**
+     * @param  array  $attributes
+     * 
+     * @return mixed
+     */
+    public function save(array $attributes);
+
+    /**
+     * @param  array  $attributes
+     *
+     * @return mixed
+     */
+    public function create(array $attributes);
+
+    /**
+     * @return mixed
+     */
+    public function query();
+
+    /**
+     * @param  int $id
+     *
+     * @return mixed
+     */
+    public function getById(int $id);
+
+    /**
+     * @param  array  $first
+     * @param  array  $create
+     *
+     * @return mixed
+     */
+    public function firstOrCreate(array $first , array $create);
+}
