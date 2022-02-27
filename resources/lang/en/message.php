e802edd1d90784b3104d52da56f394c7fc886fb9
Widiasa Mahendra <dmforaname@gmail.com>
2022-02-27 18:17:36 +0700

    message lang

---
+++ resources/lang/en/message.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+   'create' => ':X Successfully Saved',
+   'retrieve' => ':X retrieved successfully',
+   'update' => ':X Updated',
+   'saving_error' => 'Saving Error',
+   'delete_failed' => 'Failed to delete',
+   'delete_check' => 'You cant delete<br>This :X using by :Y',
+   'delete' => ':X Deleted',
+   'error' => 'error occurred',
+   'update_check' => 'You cant update<br>This :X using by :Y',
+
+];
